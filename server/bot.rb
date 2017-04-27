9c5e019206b4888e4da2d2f8967f37e86517e282
Hiroyuki Sano <sh19910711@gmail.com>
2017-04-27 09:10:55 +0000

    test closed connections

--- server/bot.rb
+++ server/bot.rb
@@ -2,9 +2,17 @@ require 'em-websocket'
 require 'json'
 
 cons = []
+closed = []
 
-def response
+def response(msg)
   data = {type: 'message', text: msg['text'], success: true}
+
+  if msg['text'] === '@bot ping'
+    data[:type] = 'bot'
+    data[:text] = 'pong'
+  end
+
+  puts data.to_json
   data.to_json
 end
 
@@ -13,8 +21,13 @@ EM::WebSocket.start(host: ENV['IP'], port: ENV['PORT']) do |con|
     cons << con
   end
 
-  con.onmessage do |msg|
-    msg = JSON.parse(msg)
-    cons.each {|con| con.send(response(msg)) }
+  con.onmessage do |jsonmsg|
+    msg = JSON.parse(jsonmsg)
+    cons.each {|con| con.send(response(msg)) unless closed.include?(con) }
+  end
+
+  con.onclose do
+    puts 'onclose'
+    closed << con
   end
 end
